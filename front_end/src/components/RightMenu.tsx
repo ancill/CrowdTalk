cdec6acff2708b30688a329c8c00820d2d10ae80
Ivan Arsenev <iv.arsenev@gmail.com>
2021-03-11 21:35:30 +0300

    Added reducer and useWindow hook

--- front_end/src/components/RightMenu.tsx
+++ front_end/src/components/RightMenu.tsx
@@ -1,4 +1,10 @@
+import { useWindowDimensions } from "../hooks/useWindowDimensions"
+
 const RightMenu = () => {
+  const { width } = useWindowDimensions()
+  if (width <= 768) {
+    return null
+  }
   return <div className="rightmenu">RightMenu</div>
 }
 
--- front_end/src/components/SideBar.tsx
+++ front_end/src/components/SideBar.tsx
@@ -1,4 +1,9 @@
+import { useWindowDimensions } from "../hooks/useWindowDimensions"
 const SideBar = () => {
+  const { width } = useWindowDimensions()
+  if (width <= 768) {
+    return null
+  }
   return <div className="sidebar">Sidebar</div>
 }
 export default SideBar
